e590044a2363f3f980e62bfcdb6d40ab00d4329a
yieme <yieme@cormea.com>
2015-02-05 14:09:06 -0700

    initial

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# logo.img
+# logo.img <img src="https://raw.githubusercontent.com/voodootikigod/logo.js/master/js.png" align="right" height="120">\index
 Logo Image Builder
+
+# Install
+
+
+```js
+npm install logo.img
+```
+
+# License: MIT
---
+++ index.js
@@ -0,0 +1,17 @@
+// logo.img - Logo Image Builder - Copyright (C) 2015 by yieme - All Rights Reserved - License: MIT
+
+;(function() {
+  'use strict';
+
+  // var convar = require('convar')
+
+  var Logo.img = function() {
+
+  }
+
+
+
+  if (typeof exports === 'object') module.exports = Logo.img // support CommonJS
+  // else if (typeof define === 'function' && define.amd) define(function() { return Logo.img }) // support AMD
+  // else this.Logo.img = Logo.img // support browser
+})();
